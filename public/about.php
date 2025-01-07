d480df0ae6bd0e16131d11ed1431efc37a348304
chandima1220 <142399459+chandima1220@users.noreply.github.com>
2025-01-07 17:33:35 +0000

    created about.php

---
+++ public/about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Us</title>
+    </head>
+<body>
+<h5>About Us</h5>
+<p>Welcome to our Flower Shop! We specialize in providing beautiful floral arrangements for all occasions, including weddings, birthdays, anniversaries, and more.</p>
+        <p>Our mission is to bring the freshest and most beautiful flowers to your doorstep. Whether you're looking for a bouquet for someone special or an elegant arrangement for your home, we are here to help!</p>
+</body>
+</html>
